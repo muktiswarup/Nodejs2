082c53dd0ed75659d2934cbb96dc53623a7b14fd
muktiswarup <csjena1999@gmail.com>
2024-08-16 21:26:55 +0530

    j

---
+++ practice2/httpmodule/index.js
@@ -0,0 +1,25 @@
+const http= require('http')
+const url= require('url')
+const server=http.createServer((req,res)=>{
+    // console.log(req);
+    // console.log(res)
+    // res.writeHead(201,{
+    //     "content-type": " application/json"
+    // }). write("Hello world i am back");
+
+   /*  if(url==='/'){
+        res.write("You are in Home page");
+    }else{
+        res.write(JSON.stringify({msz:`You are in ${req.url} page`}))
+    }
+    res.end(); */
+
+})
+
+
+
+const port= 3000;
+server.listen(port,(err)=>{
+    if(err) throw err
+    console.log('server listening')
+})
---
+++ practice2/npm/index.js
@@ -0,0 +1,12 @@
+/* const figlet= require('figlet')
+figlet("Hello Shyam",(err,data)=>{
+    if(err) throw err
+    console.log(data);
+}) */
+
+
+// this npm package is used to know the status code 
+ /* const http_status= require('http-status');
+
+ console.log(http_status[506])
+ console.log(http_status[200]) */
